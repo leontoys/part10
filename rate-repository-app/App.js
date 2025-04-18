6790e7a28dfb02b942fd6cbe6ce5abdc39311dcc
leontoys <svliyon@gmail.com>
2025-04-19 07:58:12 +1000

    10.2 added environment variable

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -2,13 +2,15 @@ import Main from "./src/components/Main";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
 import createApolloClient from "./src/utils/apolloClient";
-
+import Constants from "expo-constants";
 import React from "react";
 import { StatusBar } from "react-native-web";
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
+
   return (
     <>
       <NativeRouter>
