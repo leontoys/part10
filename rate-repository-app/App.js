a095f083ddb31b637e476a96d927c3322f3764b9
leontoys <svliyon@gmail.com>
2025-04-19 07:11:04 +1000

    added utitlity for appollo client

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,14 +1,20 @@
 import Main from "./src/components/Main";
 import { NativeRouter } from "react-router-native";
+import { ApolloProvider } from "@apollo/client";
+import createApolloClient from "./src/utils/apolloClient";
 
 import React from "react";
 import { StatusBar } from "react-native-web";
+
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
     <>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar barStyle="auto" />
     </>
