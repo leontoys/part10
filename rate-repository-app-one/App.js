813e47eaca0119fdb5fb1ccc4b8c7290c99a4e33
leontoys <svliyon@gmail.com>
2025-08-14 07:35:50 +1000

    added main component

--- rate-repository-app-one/App.js
+++ rate-repository-app-one/App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/components/Main";
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to React Native!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Main/>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
