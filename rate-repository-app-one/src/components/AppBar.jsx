bb42e6f4313acedf11419df00dcea67ef866176e
leontoys <svliyon@gmail.com>
2025-08-15 06:22:55 +1000

    10.4 added app bar

---
+++ rate-repository-app-one/src/components/AppBar.jsx
@@ -0,0 +1,29 @@
+import {View,StyleSheet, Pressable,Text} from 'react-native'
+import Constants from 'expo-constants'
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#24292e',
+    },
+    text: {
+        color: 'white',
+        margin: 'auto',
+        padding: 'auto',
+        fontSize: 20,
+        fontFamily: 'Cochin',
+        fontWeight : 'bold'
+    }
+})
+
+const AppBar = () => {
+    return (
+        <View style={styles.container}>
+            <Pressable>
+                <Text style={styles.text}>Repositories</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default AppBar
